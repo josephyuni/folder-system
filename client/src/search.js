c47f351d150d0e6b550b2dda7cdb9ea825846aba
josephyuni <74812295+josephyuni@users.noreply.github.com>
2023-06-28 10:25:07 +0800

    修改了读取pdf和word部分，尝试解决读取文件部分的问题

--- client/src/search.js
+++ client/src/search.js
@@ -3,7 +3,6 @@ const path = require('path');
 const PDFParser = require('pdf2json');
 const mammoth = require('mammoth');
 const Docxtemplater = require('docxtemplater');
-const readFileAsync = promisify(fs.readFile);
 
 // 建立索引
 async function indexFiles(directoryPath) {
@@ -56,50 +55,114 @@ async function indexFiles(directoryPath) {
   return index;
 }
 
+// 读取目录中的文件
+function readDirectory(directoryPath) {
+  return new Promise((resolve, reject) => {
+    fs.readdir(directoryPath, (error, files) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(files);
+      }
+    });
+  });
+}
 
-/**
- * 搜索文件中包含指定查询的内容
- * @param {Array<object>} files 文件对象数组
- * @param {string} query 查询关键字
- * @returns {Array<object>} 搜索结果对象数组
- */
-function searchFiles(files, query) {
+// 获取文件扩展名
+function getFileExtension(fileName) {
+  return fileName.split('.').pop().toLowerCase();
+}
+
+// 读取PDF文件内容
+function readPdf(filePath) {
+  return new Promise((resolve, reject) => {
+    const parser = new PDFParser();
+
+    parser.on('pdfParser_dataError', error => reject(error));
+    parser.on('pdfParser_dataReady', data => {
+      const pages = data.formImage.Pages;
+      const content = [];
+
+      for (const page of pages) {
+        for (const text of page.Texts) {
+          content.push(decodeURIComponent(text.R[0].T));
+        }
+      }
+
+      resolve(content.join(' '));
+    });
+
+    parser.loadPDF(filePath);
+  });
+}
+
+// 读取Docx文件内容
+function readDocx(filePath) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'binary', (error, fileData) => {
+      if (error) {
+        reject(error);
+      } else {
+        const docxData = new Uint8Array(Buffer.from(fileData));
+        const docx = new Docxtemplater();
+        docx.loadZip(docxData);
+
+        const extractedText = docx.getFullText().trim();
+        resolve(extractedText);
+      }
+    });
+  });
+}
+
+// 读取Doc文件内容
+function readDoc(filePath) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'binary', (error, fileData) => {
+      if (error) {
+        reject(error);
+      } else {
+        const docData = new Uint8Array(Buffer.from(fileData));
+        const doc = new Docxtemplater();
+        doc.loadZip(docData);
+
+        const extractedText = doc.getFullText().trim();
+        resolve(extractedText);
+      }
+    });
+  });
+}
+
+// 搜索文件内容
+function searchFiles(index, keyword) {
   const results = [];
 
-  for (const file of files) {
-    const matchedLines = file.lines.filter(line => line.content.includes(query));
-
-    if (matchedLines.length > 0) {
-      results.push({
-        path: file.filePath,
-        lines: matchedLines
-      });
+  for (const entry of index) {
+    if (entry.content.includes(keyword)) {
+      results.push(entry);
     }
   }
 
   return results;
 }
 
-/**
- * 将搜索结果保存到文本文件并下载
- * @param {Array<object>} results 搜索结果对象数组
- */
+// 保存搜索结果到文件
 function saveResultsToFile(results) {
-  let output = '';
+  let fileContent = '';
 
   for (const result of results) {
-    output += `${result.path}\n`;
-    output += '-------------------------------\n';
+    fileContent += `路径：${result.path}\n`;
+    fileContent += '-------------------------------\n';
 
     for (const line of result.lines) {
-      output += `行号${line.lineNumber}\t${line.content}\n`;
+      fileContent += `行号${line.lineNumber}\t${line.content}\n`;
     }
 
-    output += '\n';
+    fileContent += '\n';
   }
 
-  const fileName = 'search_results.txt';
-  fs.writeFile(fileName, output, 'utf8', (error) => {
+  const fileName = '搜索结果.txt';
+
+  fs.writeFile(fileName, fileContent, 'utf8', (error) => {
     if (error) {
       console.error('保存搜索结果到文件时发生错误', error);
     } else {
