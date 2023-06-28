57cef3555d25276dbd0e39de277ac9111b8caf79
josephyuni <74812295+josephyuni@users.noreply.github.com>
2023-06-29 00:28:20 +0800

    重新修改选择文件夹功能在服务端选择

---
+++ client/src/components/FileSelector.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+      <button @click="selectFolder">选择文件夹</button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    methods: {
+      selectFolder() {
+        // 处理选择文件夹逻辑
+        this.$emit('select-folder');
+      }
+    }
+  };
+  </script>
+  
